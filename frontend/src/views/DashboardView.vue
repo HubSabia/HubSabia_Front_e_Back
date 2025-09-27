ddbc337e8a547d8317a6f95b98bf6ad637684ba3
HubSabia <hubsabia@gmail.com>
2025-09-27 14:36:22 -0300

    Update DashboardView.vue

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -127,7 +127,7 @@ onMounted(buscarDados);
 
 .status-grid {
   display: grid;
-  grid-template-columns: repeat(auto, minmax(220px, 1fr));
+  grid-template-columns: repeat(2, minmax(420px, 1fr));
   gap: 25px;
   margin-bottom: 30px;
 }
