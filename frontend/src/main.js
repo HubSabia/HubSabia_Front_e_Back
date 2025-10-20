dd711e5a774d0f7e4ec7b7adff0ac1e6e7aea928
HubSabia <hubsabia@gmail.com>
2025-10-20 20:27:44 -0300

    Substituir Alertas por Notificações 03

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import '@/assets/global.css'
 import router from './router' 
-
+import Toast, { POSITION } from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import './style.css' 
 
 const app = createApp(App)
@@ -13,3 +14,20 @@ app.use(router)
 app.use(pinia)
 
 app.mount('#app')
+
+const toastOptions = {
+    position: POSITION.TOP_RIGHT, // Posição padrão das notificações
+    timeout: 5000, // Desaparecem após 5 segundos
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+};
+
+app.use(Toast, toastOptions);
