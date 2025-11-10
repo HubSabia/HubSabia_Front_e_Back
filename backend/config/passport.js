74e393132f5649394c62032a676b92dab336704f
HubSabia <hubsabia@gmail.com>
2025-11-10 17:04:59 -0300

    bug da API

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,7 +5,7 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/api/auth/google/callback'
+        callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
         // 'profile' contém as informações do usuário do Google
